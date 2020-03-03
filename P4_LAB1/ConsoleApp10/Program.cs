c5d71e02e1ae2df7147c5ab10f946a7126792821
Szymon Szałata <szymon.szalata.1@gmail.com>
2020-03-03 10:23:20 +0100

    Program z pierwszych laboratoriów
    @virouzrx

---
+++ P4_LAB1/ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace lab1netcore
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionStrig = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; 
+            using var connection = new SqlConnection(connectionStrig);
+
+            var db = new klasa_db();
+            connection.Open();
+
+
+            //db.Select(connection);
+            db.Update(connection, 1488, "Bielsko");
+            db.Select(connection);
+            connection.Close();
+        }
+
+
+    }
+}
---
+++ P4_LAB1/ConsoleApp10/klasa_db.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+namespace lab1netcore
+{
+    public class klasa_db
+    {
+        public void Select(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM region";
+            var command = new SqlCommand(sql, connection); // comand wykorzystuje komende var sql
+            SqlDataReader reader = command.ExecuteReader(); // ?
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0] + " " + reader[1]);
+            }
+            reader.Close();
+        }
+        public void Insert(SqlConnection connection, int id, string regionName)
+        {
+            var sql = "INSERT INTO Region(RegionId, RegionDescription) VALUES (@id, @regionName)";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id); // ?
+            command.Parameters.AddWithValue("@regionName", regionName); // ?
+
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+        public void Delete(SqlConnection connection, int id)
+        {
+            var sql = "DELETE FROM Region WHERE RegionID = @id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+
+        public void Update(SqlConnection connection, int id, string newRegionName)
+        {
+            var sql = "Update Region SET RegionDescription=@newRegionName WHERE RegionId=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@newRegionName", newRegionName);
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+    }
+}
